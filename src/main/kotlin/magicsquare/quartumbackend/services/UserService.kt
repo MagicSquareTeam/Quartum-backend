92bb991a839a55f31389b012e8e3c72a66cecff0
Nikita <huskyray@mail.ru>
2022-04-22 19:54:31 +0300

    add: userController, edited dto's

--- src/main/kotlin/magicsquare/quartumbackend/services/UserService.kt
+++ src/main/kotlin/magicsquare/quartumbackend/services/UserService.kt
@@ -4,14 +4,18 @@ import magicsquare.quartumbackend.exceptions.InventoryServiceException
 import magicsquare.quartumbackend.persistance.entity.Article
 import magicsquare.quartumbackend.persistance.entity.User
 import magicsquare.quartumbackend.persistance.mapper.ArticleMapper
+import magicsquare.quartumbackend.persistance.mapper.UserMapper
 import magicsquare.quartumbackend.persistance.repository.UserRepository
 import magicsquare.quartumbackend.web.dto.ArticleDto
+import magicsquare.quartumbackend.web.dto.UserDto
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
-    private val repository: UserRepository
+    private val repository: UserRepository,
+    private val userMapper: UserMapper,
+    private val articleMapper: ArticleMapper
 ) {
 
     fun findById(id: Long): User {
@@ -26,5 +30,27 @@ class UserService(
 
     fun getUserArticles(id: Long): MutableSet<Article> {
         return repository.findById(id).get().articles
+    }
+
+    fun addUserSub(userId: Long, userSubId: Long) {
+        val user: User = repository.findById(userId).get()
+        user.users_subscriptions.add(repository.findById(userSubId).get())
+        repository.save(user)
+    }
+
+    fun getUserSubs(userId: Long): MutableSet<User> {
+        return repository.findById(userId).get().users_subscriptions
+    }
+
+    fun getUserData(userId: Long): UserDto {
+        return userMapper.toDto(repository.findById(userId).get())
+    }
+
+    fun getSubsArticles(userId: Long): MutableSet<ArticleDto> {
+        val articles: MutableSet<ArticleDto> = mutableSetOf()
+        repository.findById(userId).get().users_subscriptions.forEach {
+            articles.addAll(it.articles.map { article -> articleMapper.toDto(article) }.toMutableSet())
+        }
+        return articles
     }
 }
